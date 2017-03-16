0918b21008c0fed8cb7597cb6b6064c52f56b763
William Wolf <wwolf@shopkeep.com>
2017-03-16 19:21:32 +0000

    add method to build stan model

---
+++ R/buildModel.R
@@ -0,0 +1,24 @@
+#' Build Bayesian ordered categorical linear model for estimation an ordered multinomial distribution
+#'
+#' @param feedback A data-frame column of ordered integer scores
+#' @param outcomes A vector of the unique categories (e.g. c(1, 2, 3, 4))
+#' @param cores The number of cores to use in the Stan sampler
+#' @param iter The number of iterations to use in the Stan sampler
+#' @param warmup The number of warmup iterations to use in the Stan sampler
+#' @return The fitted `stanfit` model object
+#' @export
+
+buildModel <- function(feedback, outcomes, chains = 1, cores = 1, iter = 1000, warmup = 1000) {
+    data <- list(obs = feedback, N = length(feedback))
+    initial_values <- list(cutpoints = sort(outcomes))
+    fit <- sampling(
+        object = stanmodels$model,
+        data = data,
+        init = rep(initial_values, chains),
+        chains = chains,
+        cores = cores,
+        iter = iter,
+        warmup = warmup
+    )
+    return( fit )
+}
